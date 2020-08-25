79619289743fa6356e3f472fdd37af43cb2232a3
Anthony Zheng <anthonyzheng0290@gmail.com>
2020-08-25 01:44:45 -0400

    v1 build

---
+++ precache-manifest.66c9aa890c95db8252f64b3ca9fce7bf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1b0a9c23427d35e08f187aaf340c5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8283687d767bfee856f7",
+    "url": "/static/css/main.f48852a6.chunk.css"
+  },
+  {
+    "revision": "75d1a493864372423c63",
+    "url": "/static/js/2.f4741bce.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/static/js/2.f4741bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8283687d767bfee856f7",
+    "url": "/static/js/main.2b637809.chunk.js"
+  },
+  {
+    "revision": "66defab84462cf699330",
+    "url": "/static/js/runtime-main.fea7322e.js"
+  },
+  {
+    "revision": "3abde616ea2c2652c9c1130181e1aa93",
+    "url": "/static/media/Bold.3abde616.otf"
+  },
+  {
+    "revision": "f38dce0a923f80b613ee1c7450c7a807",
+    "url": "/static/media/Regular.f38dce0a.otf"
+  },
+  {
+    "revision": "fa9fdf3624cdb506adadb743d34dedb3",
+    "url": "/static/media/RegularItalic.fa9fdf36.otf"
+  },
+  {
+    "revision": "9b1cbed4f1e6a2ef9822e944e0332b26",
+    "url": "/static/media/Semibold.9b1cbed4.otf"
+  }
+]);
