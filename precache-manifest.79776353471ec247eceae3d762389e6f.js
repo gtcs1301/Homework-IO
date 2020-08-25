e73690d0ffda4f26bdf96fae2313389acce4fc3a
Anthony Zheng <anthonyzheng0290@gmail.com>
2020-08-25 01:51:08 -0400

    v2 build

---
+++ precache-manifest.79776353471ec247eceae3d762389e6f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faaebf8865415a33cca35c0355e55f1e",
+    "url": "/Homework-IO/index.html"
+  },
+  {
+    "revision": "e28e28e84f7503941712",
+    "url": "/Homework-IO/static/css/main.f3730d04.chunk.css"
+  },
+  {
+    "revision": "75d1a493864372423c63",
+    "url": "/Homework-IO/static/js/2.f4741bce.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Homework-IO/static/js/2.f4741bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28e28e84f7503941712",
+    "url": "/Homework-IO/static/js/main.2b637809.chunk.js"
+  },
+  {
+    "revision": "d5191a8b2e5745fcd44a",
+    "url": "/Homework-IO/static/js/runtime-main.797b26df.js"
+  },
+  {
+    "revision": "3abde616ea2c2652c9c1130181e1aa93",
+    "url": "/Homework-IO/static/media/Bold.3abde616.otf"
+  },
+  {
+    "revision": "f38dce0a923f80b613ee1c7450c7a807",
+    "url": "/Homework-IO/static/media/Regular.f38dce0a.otf"
+  },
+  {
+    "revision": "fa9fdf3624cdb506adadb743d34dedb3",
+    "url": "/Homework-IO/static/media/RegularItalic.fa9fdf36.otf"
+  },
+  {
+    "revision": "9b1cbed4f1e6a2ef9822e944e0332b26",
+    "url": "/Homework-IO/static/media/Semibold.9b1cbed4.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66c9aa890c95db8252f64b3ca9fce7bf.js"
+  "/Homework-IO/precache-manifest.79776353471ec247eceae3d762389e6f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Homework-IO/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
